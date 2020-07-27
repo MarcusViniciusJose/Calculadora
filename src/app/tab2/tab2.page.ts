7ccc63a6557a3d76776abddfd1534a3a231a89c7
MarcusViniciusJose <marcus.jose@etec.sp.gov.br>
2020-07-27 00:20:32 -0300

    Calc Completa
    Parte 1:  Criação do aplicativo e personalização, inicio da calculadora simples;
    Parte 2: Criação de variaveis globais, criação de metodos de calculos e exibir resultado;
    Parte 3: Inicio da calculadora normal deixando ela mais funcional com comandos;
    Parte 4: Metodos para adicionar operações e variaveis globais;
    Parte 5: Adicionado o metodo  para apagar tudo e apagar último;
    Parte 6: Ajuste dos comandos logicos;
    Parte 7: Instalação da biblioteca Math.js e alert mostrar para o desenvolvedor.

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,90 @@
+import { Component } from '@angular/core';
+import { evaluate } from 'mathjs';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  public calculo = '';
+  public resultado: string; 
+  private ponto = false;
+  private operacoes = ['+', '-', '*', '/'];
+  constructor(public alertController: AlertController) {}
+
+  public adicionarNumero(valor: string){
+
+    if(this.resultado){
+      this.apagarTudo();
+    }
+
+    this.calculo = this.calculo + valor;
+  }
+
+  public adicionarPonto(){
+    if(this.ponto){
+      return;
+    }
+    this.calculo += ".";
+    this.ponto = true;
+  }
+
+  public adicionarOperacao(operador: string){
+    
+    if(this.resultado){
+      this.calculo = this.resultado.toString();
+      this.resultado = null;
+    }
+
+    const ultimo = this.calculo.slice(-1);
+    if(this.operacoes.indexOf(ultimo) > -1){
+      return;
+    }
+
+    this.calculo += operador;
+    this.ponto = false;
+    
+  }
+
+  public apagarTudo(){
+    this.calculo = '';
+    this.resultado = null;
+    this.ponto = false;
+
+  }
+
+  public apagarUltimo(){
+    const ultimo = this.calculo.slice(-1);
+    if(ultimo == '.'){
+      this.ponto = false;
+    }
+    this.calculo = this.calculo.slice(0, -1);
+  }
+
+  public calcularResultado(){
+    try{
+    
+    this.resultado = evaluate(this.calculo);
+    }catch (e) {
+      this.resultado = '';
+      this.presentAlert('ERRO!!!', 'CALCULO INVÁLIDO, VEREFIQUE!')
+
+    }
+  }
+
+  async presentAlert(titulo: string, mensagem: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensagem,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+
+
+}
